25ecb2274d511f67d4e4d5da25c688d7ad74074c
Gilberto Asuaje <gilberto.asuaje@payco.co>
2020-11-29 18:08:51 -0400

    ultimos cambios en routes

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -13,9 +13,7 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 
-/**
- * @Route("/users")
- */
+
 class UsersController extends AbstractController
 {
     private $usersRepository;
@@ -24,19 +22,9 @@ class UsersController extends AbstractController
     {
         $this->usersRepository = $usersRepository;
     }
-    /**
-     * @Route("/", name="users_index", methods={"GET"})
-     */
-    public function index(UsersRepository $usersRepository): Response
-    {
-        return $this->render('users/index.html.twig', [
-            'users' => $usersRepository->findAll(),
-        ]);
-    }
 
-    /**
-     * @Route("/new", name="users_new", methods={"GET","POST"})
-     */
+
+
     public function new(Request $request): Response
     {
         $data = json_decode($request->getContent(),true);
@@ -55,47 +43,5 @@ class UsersController extends AbstractController
       
     }
 
-    /**
-     * @Route("/{id}", name="users_show", methods={"GET"})
-     */
-    public function show(Users $user): Response
-    {
-        return $this->render('users/show.html.twig', [
-            'user' => $user,
-        ]);
-    }
 
-    /**
-     * @Route("/{id}/edit", name="users_edit", methods={"GET","POST"})
-     */
-    public function edit(Request $request, Users $user): Response
-    {
-        $form = $this->createForm(UsersType::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('users_index');
-        }
-
-        return $this->render('users/edit.html.twig', [
-            'user' => $user,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @Route("/{id}", name="users_delete", methods={"DELETE"})
-     */
-    public function delete(Request $request, Users $user): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->remove($user);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('users_index');
-    }
 }
